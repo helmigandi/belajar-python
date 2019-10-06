b6f14d8b3e262573c6f5e817f251dc546dc0b9f9
Helmi <sugandihelmi@gmail.com>
2019-10-06 08:55:33 +0700

    Membuat perulangan tanpa tahu berapa angka perulangannya

---
+++ whileStatements.py
@@ -0,0 +1,9 @@
+# You can make a block of code execute over and over again 
+# with a while statement. The code in a while clause will 
+# be executed as long as the while statement’s condition is True.
+
+name = ''
+while name != 'helmi':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')

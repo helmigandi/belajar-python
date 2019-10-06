65318aee52c0625e52e33b17d1e26f284a0ea19f
Helmi <sugandihelmi@gmail.com>
2019-10-06 08:57:45 +0700

    Membuat Default Argument Function

---
+++ DAV.py
@@ -0,0 +1,17 @@
+# Default Argument Values
+# The most useful form is to specify a default value for one 
+# or more arguments. This creates a function that can be called
+# with fewer arguments than it is defined to allow.
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+ask_ok('Do you really want to quit? ')

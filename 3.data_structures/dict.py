7b8da3770e3d0c1eb72bc42f00fe4bc89660323d
Helmi <sugandihelmi@gmail.com>
2019-10-23 15:49:53 +0700

    Menambahkan Dictionary Comprehension

--- 3.data_structures/dict.py
+++ 3.data_structures/dict.py
@@ -206,3 +206,13 @@ MLB_team = {
 # False
 'Toronto' not in MLB_team
 # True
+
+
+
+# Dictionary Comprehension
+# dict_variable = {key:value for (key,value) in dictonary.items()}
+
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# Double each value in the dictionary
+double_dict1 = {k:v*2 for (k,v) in dict1.items()}
+print(double_dict1)
